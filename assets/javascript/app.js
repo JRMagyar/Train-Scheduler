453c6760d3e4c4a2f5456758053f56df9dc68950
Julia Magyar <magyar.juliar@gmail.com>
2018-12-09 16:00:08 -0500

    html added

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//document ready
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDjebO6kUWFDEJ05AYQPHFTfymHmjdRId8",
+    authDomain: "train-scheduler-6ac08.firebaseapp.com",
+    databaseURL: "https://train-scheduler-6ac08.firebaseio.com",
+    projectId: "train-scheduler-6ac08",
+    storageBucket: "train-scheduler-6ac08.appspot.com",
+    messagingSenderId: "154981265907"
+  };
+  firebase.initializeApp(config);
