25ba2aa3868ff3138e8b846016ea3b859a05b7dc
Julia Magyar <magyar.juliar@gmail.com>
2018-12-09 20:21:08 -0500

    currently just needs calculations working. commented out for loop to keep from crashing browser

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 //document ready
 
 // Initialize Firebase
@@ -10,3 +11,54 @@ var config = {
     messagingSenderId: "154981265907"
   };
   firebase.initializeApp(config);
+  var database = firebase.database();
+
+//When user hits submit
+$("#add-train").on("click", function(event){
+    event.preventDefault();
+    var newTrain = $("#train-name-input").val().trim();
+    var newDestination = $("#train-destination-input").val().trim();
+    var newTime = $("#train-time-input").val();
+    var newFreq = $("#train-freq-input").val();
+
+    database.ref().push({
+        trainName: newTrain,
+        trainDestination: newDestination,
+        trainTime: newTime,
+        trainFreq: newFreq,
+    })
+})
+
+//calls database when new info added
+database.ref().on("child_added", function(snapshot){
+    //create new row and add info from database
+    newRow = $("<tr>");
+    newRow.append("<td>" + snapshot.val().trainName + "</th>");
+    newRow.append("<td>" + snapshot.val().trainDestination + "</th>");
+    newRow.append("<td>" + snapshot.val().trainFreq + "</th>");
+    
+    //calculate next arrival
+    //add (frequency) minutes to the start time until it is past current time. If it is past midnight next train time is first run time.
+    start = snapshot.val().trainTime;
+    freq = snapshot.val().trainFreq
+    now = moment().format("HH:mm")
+    // console.log(start)
+    // console.log(freq)
+    // console.log(now)
+    // for(i = start; moment(i, "HH:mm").isBefore(moment(now,"HH:mm")) == true ; moment(i, "HH:mm").add(parseInt(freq), "m").format("HH:mm")){
+    //     console.log(moment(i, "HH:mm").format("HH:mm"))
+    // }
+
+    
+
+    //calculate minutes away
+    //convert first time and next arrival to minutes. subtract first from next and %frequency
+
+    $("#schedule").append(newRow)
+})
+
+
+
+
+}
+
